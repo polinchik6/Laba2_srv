10a9f3635343681f29f3e138f0ccc358f41dd328
Polina <polinawey2@gmail.com>
2025-11-16 14:49:21 +0300

    Выполнение

---
+++ лаба2/лаба2.cpp
@@ -0,0 +1,34 @@
+﻿#include <thread>
+#include <mutex>
+#include <string>
+#include <iostream>
+#include <chrono>
+
+std::mutex m;
+
+void Func(std::string name) {
+    long double i = 0;
+    auto start = std::chrono::steady_clock::now();
+    auto end = start + std::chrono::seconds(1);
+
+    while (std::chrono::steady_clock::now() < end) {
+        i += 1e-9; 
+    }
+
+    m.lock();
+    std::cout << name << ". " << i << std::endl;
+    m.unlock();
+}
+
+int main() {
+    std::thread thread1(Func, "t1");
+    std::thread thread2(Func, "t2");
+    std::thread thread3(Func, "t3");
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    system("pause");
+    return 0;
+}
